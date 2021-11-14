5e1482cc3780f42d84c3bc19cb5fa6e43e2b1d58
Arne Coomans <app+github@arnecoomans.nl>
2021-11-14 16:56:29 +0100

    improved order of core module loading, added core path and app_name functions

--- framework_core.py
+++ framework_core.py
@@ -27,13 +27,29 @@ class Framework:
     self.modules = {}
     # Load Core Extended Functionality
     self.log      = Logging(self)
+    self.files    = Files(self)
     self.config   = Config(self)
     self.interact = Interact(self)
-    self.files    = Files(self)
+    
+    
+
+    # Core Configuration Shortcuts
+    self.core_path = self.files.getPath(os.path.dirname(os.path.abspath(__file__)))
+    self.app_name = self.getAppName()
+    #self.log.print(self.app_name)
+
+    # Initialisation
+    self.config.parseArgumentParser()
 
     self.log.debug(['Core is done with loading:', '* core;', '* logging;', '* configuration;', '* interaction.'])
     
     
+  ## Context management
+  def getAppName(self):
+    return self.files.getFile(sys.argv[0]).stem
+  def getCorePath(self):
+    return self.files.getPath(os.path.dirname(os.path.abspath(__file__)))
+  
   ## Module management
   ### getModule
   #   @description returns the loaded instance of a module
