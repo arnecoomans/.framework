977d50d7e1026d423051a221b55420508edd661e
Arne Coomans <app+github@arnecoomans.nl>
2021-11-26 22:06:14 +0100

    Introduced writing to logfile in logging module

--- framework_logging.py
+++ framework_logging.py
@@ -5,6 +5,7 @@
 #
 import sys, os
 from datetime import datetime
+#import logging # https://docs.python.org/3/library/logging.html
 
 # Ensure script is embedded and not called directly.
 if __name__ == "__main__":
@@ -22,28 +23,46 @@ class Logging(Boilerplate):
     # Run Boilerplate initialisation
     super().__init__(framework)
     # Prepare reference containers
-    self.logfile_target = None
-    self.logfile_suffix = '.log'
+    self.logfile_target           = None
+    self.logfile_suffix           = '.log'
+    self.logfile_write_mode       = 'w'    # Supports a for append en w for (over)write
+    self.logfile                  = None   # Reference to file in open
     # Prepare configurable values
-    self.min_log_level = 2
-    self.min_logfile_level = 4
-    self.max_display_buffer_size = 16
+    self.min_log_level            = 2
+    self.min_logfile_level        = 0
+    self.max_display_buffer_size  = 16
     # Prepare data containers
-    self.buffer = []        # For pushing to screen
-    self.print_buffer = []  # For pushing to file
+    self.buffer                   = []     # For pushing to screen
+    self.file_buffer              = []     # For pushing to file
     # Development and debugging
-  
+
+  def __del__(self):
+    if self.logfile is not None: 
+      # Try to flush buffer
+      try:
+        self.flush()
+      except:
+        pass
+      # Try to close the write connection to the logfile
+      try:
+        self.logfile.close()
+      except:
+        pass
+
   # Log file initialisation
   def logFileInit(self):
     # Check if logging to file should be enabled.
     if self.getArgument('logfile'):
-      # Log file is enabled
-      # Set logfile target
+      # Log file is enabled: Set logfile target
       self.setLogFileTarget(target=self.getArgument('logfile'))
       # Set logfile display level
-      if not self.getArgument('logfile-verbose') is None:
-        self.setLogFileVerbose(self, level=self.getArgument('logfile-verbose'))
-  
+      if not self.getArgument('logfile_verbose') is None:
+        self.setLogFileVerbose(level=self.getArgument('logfile_verbose'))
+      # Set logfile write mode
+      if self.getArgument('logfile_append') is True:
+        self.setLogFileWriteMode('append')
+    # If argument is not set, no action is to be taken
+
   #   Log file functions
   def setLogFileTarget(self, target):
     # If no filename is supplied, assume default format of log file name
@@ -51,16 +70,59 @@ class Logging(Boilerplate):
       target = self.getFile(self.getDate() + '-' + self.framework.getAppName())
     else:
       # Log file name is supplied
-      target = self.getFile(target)
+      target = self.getFile(target, exists=False)
     # Use suffix if supplied, else add locally configured default
     # Store target in local storage
     self.logfile_target = target if len(target.suffixes) > 0 else target.with_suffix(self.logfile_suffix)
     # Log intended file usage.
     self.throw_notice('Log to file: Logging to: \'' + str(self.logfile_target.name) + '\'.')
 
-  def setLogFileVerbose(self, level):
-    pass
+  def setLogFileVerbose(self, level=None):
+    # If an invalid level is supplied, use default log level
+    if not type(level) is int:
+      level = self.min_logfile_level
+    # Use self.normalise_level to normalise level between 1 and 5.
+    # However, level 0 should also be supported as shortcut to only show level 2 content and not level 1 errors.
+    # So if level is smaller than 0, use 0, else use normalized level
+    level = 0 if level <= 0 else self.normalize_level(level)
+    if not level == self.min_logfile_level:
+      self.debug('Changed logfile display level from \'' + str(self.min_logfile_level) + '\' to \'' + str(level) + '\'.')
+      self.min_logfile_level = level
+    
+  def setLogFileWriteMode(self, mode='w'):
+    # Supports 'a' or 'append' as mode to set append mode
+    # Defaults to 'w' (over)write mode
+    self.logfile_write_mode = 'a' if mode[:1] == 'a' else 'w'
+  
+  def sendLineToLogFile(self, line):
+    if self.logfile_target is not None:
+      pre = str(line['datetime'])[:-4] + ' (' + self.getTextualLevel(line['level']) + '): '
+      for row in line['content']:
+        if line['level'] == 2:
+          pre = ''
+        self.getLogFile().write(pre + row + "\n")
+        pre = ' '*len(pre)
 
+  def getLogFile(self):
+    if self.logfile is None:
+      self.logfile = open(self.logfile_target, self.logfile_write_mode)
+      if self.min_logfile_level >= 3:
+        self.logfile.write(str(datetime.now())[:-4] + ': Running app \'' + self.framework.getAppName() + '\' in \'' + str(self.getPath()) + "'\n")
+      if self.min_logfile_level == 5:
+        self.logfile.write(str(datetime.now())[:-4] + ': $ \'' + ' '.join(sys.argv) + '\'.' + "\n")
+    return self.logfile
+
+  def getTextualLevel(self, level):
+    if level == 1:
+      return ' ERROR '
+    elif level == 2:
+      return '       '
+    elif level == 3:
+      return 'WARNING'
+    elif level == 4:
+      return 'NOTICE '
+    else:
+      return ' DEBUG '
 
   ##  Log Buffer
   ### Add
@@ -72,12 +134,19 @@ class Logging(Boilerplate):
     # Normalize input
     level = self.normalize_level(level)
     content = self.normalize_content(content)
-    # Add normalized content to buffer
+    # Add normalized content to display-buffer
     self.buffer.append( { 'content': content,
                           'level': level,
                           'datetime': datetime.now() # Use datetime and not framework date module 
                                                      # because timing keeps changing.
                         } )
+    # Add normalized content to file-buffer
+    self.file_buffer.append( { 'content': content,
+                               'level': level,
+                               'datetime': datetime.now() # Use datetime and not framework date module 
+                                                          # because timing keeps changing.
+                            } )
+    # Flush display buffer if max buffer size is reached
     if len(self.buffer) > self.max_display_buffer_size:
       self.flush()
     return True
@@ -89,8 +158,14 @@ class Logging(Boilerplate):
   #   @output (boolean)
   def flush(self) -> True:
     for row in self.buffer:
+      # Process displaying content to screen
       if row['level'] <= self.min_log_level:
-        self.send_line_to_display(row)
+        self.sendLineToDisplay(row)
+      # Process writing content to file
+      if row['level'] <= self.min_logfile_level:
+        self.sendLineToLogFile(row)
+      elif self.min_logfile_level == 0 and row['level'] == 2:
+        self.sendLineToLogFile(row)
     self.buffer = []
 
   ##  Shortcuts
@@ -114,8 +189,9 @@ class Logging(Boilerplate):
   #   @description normalizes input level as integer within boundaries
   #   @input level (integer)
   #   @output (ineger) [1:5]
-  def normalize_level(self, level):
+  def normalize_level(self, level=5):
     return level if level in range(1,5) else 5
+
   ### Normalize content
   #   @description normalizes input to a uniform format while allowing flexible input
   #   @input content (string|list|dict|integer|boolean)
@@ -162,27 +238,23 @@ class Logging(Boilerplate):
     elif level >5:
       level = 5
     if not level == self.min_log_level:
-      self.debug('Changed display level from ' + str(self.min_log_level) + ' to ' + str(level) + '.')
+      self.debug('Changed display level from \'' + str(self.min_log_level) + '\' to \'' + str(level) + '\'.')
       self.min_log_level = level
 
 
 
   ##  Output validation
-  def send_line_to_display(self, line):
+  def sendLineToDisplay(self, line):
     # Prepend the first line with characterisation
-    pre = ''
-    if line['level'] == 1:
-      pre = '[! Error] '
-    elif line['level'] == 3:
-      pre = '[w] '
-    elif line['level'] == 4:
-      pre = '[n] '
-    elif line['level'] == 5:
-      pre = str(line['datetime'])[:-4] + ' '
+    pre = pre = str(line['datetime'])[:-4] + ' ' if line['level'] == 5 else ''
+    pre += '[' + self.getTextualLevel(line['level']) + '] '
+    pre = '' if line['level'] == 2 else pre
     # Print all lines within the content
     for row in line['content']:
       print(pre + row)
       # Reset the prepend to empty space in the same width as the original content
       # This ensures that multiline messages are prepended only once.
       pre = ' '*len(pre)
+  
+  
     
