c553d1a0f96bc63a90f87ba793596a9af25f9864
Arne Coomans <app+github@arnecoomans.nl>
2021-12-12 07:48:34 +0100

    included --colour option adding colours to the row descriptors

--- framework_interact.py
+++ framework_interact.py
@@ -4,6 +4,7 @@
 # Adds bufferd output handling and filtering
 #
 import sys, os
+from pathlib import PosixPath, posixpath
 
 # Ensure script is embedded and not called directly.
 if __name__ == "__main__":
@@ -84,3 +85,74 @@ class Interact(Boilerplate):
       self.questions['reference'] = {'question': question, 'answer': answer, 'suggestion': suggestion}
     # Return processed answer
     return answer
+
+
+  def ask2(self, question=None, suggestion=None, reference=None):
+    # Verify Question
+    if question is None or question is False or len(str(question)) == 0:
+      # No question is supplied.
+      self.throw_warning('Interact.ask: Interaction is initiated but no question formulated.')
+      # Return suggestion if supplied
+      return self.get_suggested_answer(None, suggestion) if suggestion is not None else False
+    # Question is given
+    # Build display question
+    display_question   = self.get_display_question(question)
+    display_suggestion = self.get_display_suggestion(suggestion)
+    display_answer     = self.get_display_answer(self.getArgument(reference))
+
+    if len(display_suggestion) > 0:
+      display_suggestion = ' (' + display_suggestion + ')'
+    # Display processed question
+    self.print(display_question + display_suggestion + ': ' + display_answer)
+    
+
+
+  def get_display_question(self, question):
+    question = question if question[-1:] == '?' else question + '?'
+    # Force fist letter to be uppercase
+    if not question[:1].isupper():
+      question = question[:1].upper() + question[1:]
+    return question 
+
+
+  def get_display_suggestion(self, suggestion=None, underline_default=True):
+    if type(suggestion) == str:
+      return suggestion
+    elif type(suggestion) is list:
+      result = []
+      for item in suggestion:
+        result.append(self.get_display_suggestion(item).strip())
+      if len(result) > 0 and underline_default is True:
+        result[0] = "\033[92m" + result[0] + "\033[0m"
+      return ' / '.join(result)
+    elif type(suggestion) is dict:
+      result = []
+      for item in suggestion.values():
+        result.append(self.get_display_suggestion(item).strip())
+      if len(result) > 0 and self.getArgument('coloured') is True:
+        result[0] = "\033[92m" + result[0] + "\x1B[0m"
+      return ' / '.join(result)
+    elif type(suggestion) is PosixPath:
+      if str(self.getPath()) in str(suggestion):
+        return '.' + str(suggestion).replace(str(self.getPath()), '')
+      else:
+        return str(suggestion)
+    return ''
+
+  def get_display_answer(self, answer=None):
+    if answer is not None and answer is not False and len(str(answer)) > 0:
+      if self.getArgument('coloured') is True:
+        return str(answer + ' (' + "\033[94m" + 'from command line argument' + "\x1B[0m" + ')')
+      else:
+        return str(answer + ' (from command line argument)')
+    return ''
+
+  
+  def get_suggested_answer(self, answer, suggestion=None):
+    # If no suggestion is given, return the answer. No further processing is required
+    if suggestion is None or suggestion is False or len(str(suggestion)) == 0:
+      return answer
+    # A suggestion is given
+
+    if type(suggestion) == str:
+      suggestion = [suggestion]
